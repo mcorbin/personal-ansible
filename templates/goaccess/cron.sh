4738a40f7181d33caf1d695a18abf0b043f369ed
mcorbin <corbin.math@gmail.com>
2019-06-28 09:43:39 +0200

    goaccess: exclude health.html

--- templates/goaccess/cron.sh
+++ templates/goaccess/cron.sh
@@ -4,7 +4,7 @@ cp -r /var/log/nginx /tmp/nginx-logs
 
 /bin/zcat /tmp/nginx-logs/access.log.*.gz > /tmp/nginx-logs/access.log.unzipped
 
-/bin/cat /tmp/nginx-logs/access.log.unzipped /tmp/nginx-logs/access.log /tmp/nginx-logs/access.log.1 | grep -v health/ | /usr/bin/goaccess -g -a > /var/www/blogstats/report-$(date +%Y-%m-%d-%H-%M-%S).html
+/bin/cat /tmp/nginx-logs/access.log.unzipped /tmp/nginx-logs/access.log /tmp/nginx-logs/access.log.1 | grep -v health/ | grep -v health.html | /usr/bin/goaccess -g -a > /var/www/blogstats/report-$(date +%Y-%m-%d-%H-%M-%S).html
 
 rm -rf /tmp/nginx-logs
 
